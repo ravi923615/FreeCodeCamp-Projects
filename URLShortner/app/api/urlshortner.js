95cac6cd9bdce537f6977047a31c425f43660f2b
Ravi Kumar <ravi923615@gmail.com>
2016-05-03 22:56:27 +0530

    updated few function

--- URLShortner/app/api/urlshortner.js
+++ URLShortner/app/api/urlshortner.js
@@ -42,4 +42,26 @@ module.exports = function(app,db){
 			console.log('Saved the url'+result);
 		});
 	}
+
+	function findURL(link,db,res){
+		var urlsave = db.collection('urlsave');
+		urlsave.findOne({
+			"short_url":link
+		},function(err,result){
+			if (err) throw err;
+			if(result){
+				res.redirect(result.original_url);
+			}else{
+				res.send({
+					"error":"This URL is not present in DB."
+				});
+			}
+		});
+	}
+
+	function validateURL(url){
+		var regex = /^(?:(?:https?|ftp):\/\/)(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,}))\.?)(?::\d{2,5})?(?:[/?#]\S*)?$/i;
+		return regex.test(url);
+			}
+
 }
