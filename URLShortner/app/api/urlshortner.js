5709076ad293b39ac538eb8929691fea5b60b3f0
Ravi Kumar <ravi923615@gmail.com>
2016-07-30 23:21:42 +0530

    Completed Developement of URL Shortner App

--- URLShortner/app/api/urlshortner.js
+++ URLShortner/app/api/urlshortner.js
@@ -1,67 +1,81 @@
 'use strict';
-module.exports = function(app,db){
-	app.route('/:url')
-		.get(handleGet);
+module.exports = function(app, db) {
 
-	app.get('/new/:url',handlePost);
+  app.route('/:url')
+    // Check and retrieve url to redirect if it exist.
+    .get(handleGet);
 
-	function handleGet(req,res){
-		var url = process.env.APP_URL + req.param.url;
-		if(url != process.env.APP_URL + 'favicon.ico'){
-			findURL(url,db,res);
-		}
-	}
+  app.get('/new/:url*', handlePost);
 
-	function handlePost(req,res){
-		var url = req.url.slice(5);
-		var urlObj = {};
-		if(validateURL(url)){
-			urlObj = {
-				"original_url": url,
-				"short_url": process.env.APP_URL + newLink()
-			};
-			res.send(urlObj);
-			save(urlObj,db);
-		}else{
-			urlObj = {
-				"error": "Wrong URL format, Please send the corrected format"
-			};
-			res.send(urlObj);
-		}
-	}
+  function handleGet(req, res) {
+    var url = process.env.APP_URL + req.params.url;
+    if (url != process.env.APP_URL + 'favicon.ico') {
+      findURL(url, db, res);
+    }
+  }
 
-	function newLink(){
-		var number = Math.floor(100000+Math.random()*900000);
-		return num.to_string().substring(0,4);
-	}
+  function handlePost(req, res) {
+    // Create short url, store and display the info.
+    var url = req.url.slice(5);
+    var urlObj = {};
+    if (validateURL(url)) {
+      urlObj = {
+        "original_url": url,
+        "short_url": process.env.APP_URL + linkGen()
+      };
+      res.send(urlObj);
+      save(urlObj, db);
+    } else {
+      urlObj = {
+        "error": "Wrong url format, make sure you have a valid protocol and real site."
+      };
+      res.send(urlObj);
+    }
+  }
 
-	function save(obj,db){
-		var urlsave = db.collection('urlsave');
-		urlsave.save(obj,function(err,result){
-			if(err) throw err;
-			console.log('Saved the url'+result);
-		});
-	}
+  function linkGen() {
+    // Generates random four digit number for link
+    var num = Math.floor(100000 + Math.random() * 900000);
+    return num.toString().substring(0, 4);
+  }
 
-	function findURL(link,db,res){
-		var urlsave = db.collection('urlsave');
-		urlsave.findOne({
-			"short_url":link
-		},function(err,result){
-			if (err) throw err;
-			if(result){
-				res.redirect(result.original_url);
-			}else{
-				res.send({
-					"error":"This URL is not present in DB."
-				});
-			}
-		});
-	}
+  function save(obj, db) {
+    // Save object into db.
+    var sites = db.collection('sites');
+    sites.save(obj, function(err, result) {
+      if (err) throw err;
+      console.log('Saved ' + result);
+    });
+  }
 
-	function validateURL(url){
-		var regex = /^(?:(?:https?|ftp):\/\/)(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,}))\.?)(?::\d{2,5})?(?:[/?#]\S*)?$/i;
-		return regex.test(url);
-			}
+  function findURL(link, db, res) {
+    // Check to see if the site is already there
+    var sites = db.collection('sites');
+    // get the url
+    sites.findOne({
+      "short_url": link
+    }, function(err, result) {
+      if (err) throw err;
+      // object of the url
+      if (result) {
+        // we have a result
+        console.log('Found ' + result);
+        console.log('Redirecting to: ' + result.original_url);
+        res.redirect(result.original_url);
+      } else {
+        // we don't
+        res.send({
+        "error": "This url is not onteh database."
+      });
+      }
+    });
+  }
 
-}
+  function validateURL(url) {
+    // Checks to see if it is an actual url
+    // Regex from https://gist.github.com/dperini/729294
+    var regex = /^(?:(?:https?|ftp):\/\/)(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,}))\.?)(?::\d{2,5})?(?:[/?#]\S*)?$/i;
+    return regex.test(url);
+  }
+
+};
